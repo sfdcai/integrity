51df5d8cbf393e9b5c1cc7484c5bb67bf43d3929
Red Zebra <tworedzebras@gmail.com>
2025-11-18 09:58:04 +0000

    cleanup

--- deepguard-app/backend/app/__init__.py
+++
@@ -1,3 +0,0 @@
-from app import models, schemas
-
-__all__ = ["models", "schemas"]
--- deepguard-app/backend/app/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from app.api import wells, tasks
-
-api_router = APIRouter()
-api_router.include_router(wells.router)
-api_router.include_router(tasks.router)
